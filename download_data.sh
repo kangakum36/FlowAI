010c62c7e7afd7c0a09a9f348ea7a1959fed9736
kangakum36 <kangakum@gmail.com>
2020-12-14 13:34:38 -0500

    added scripts dir

--- download_data.sh
+++
@@ -1,2 +0,0 @@
-rm -r ./data/FlowCasesDeidentify120519
-gsutil cp -r gs://flowai-data/FlowCasesDeidentify120519 ./data/
---
+++ scripts/download_data.sh
@@ -0,0 +1,3 @@
+cd ..
+rm -r ./data/FlowCasesDeidentify120519
+gsutil -m cp -r gs://flowai-data/FlowCasesDeidentify120519 ./data/
