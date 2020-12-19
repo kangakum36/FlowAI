2f944c4da5bed805b33944a1e34539c087b3ac3f
Akum Singh Kang <kangakum@gmail.com>
2020-12-18 23:12:47 -0500

    gridsearch xgb

--- models/xgb.py
+++ models/xgb.py
@@ -5,6 +5,8 @@ from sklearn.model_selection import KFold
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score, cross_validate
 from sklearn.metrics import make_scorer, accuracy_score, precision_score, recall_score, f1_score
+from sklearn.model_selection import GridSearchCV
+import pickle
 
 class XGB(BaseModel):
 
@@ -32,8 +34,35 @@ class XGB(BaseModel):
         results = cross_validate(self.model, self.dataset, self.labels, cv=kfold, scoring=scoring)
         print(results)
 
-        print("CrossVal Accuracy: %.2f%% (%.2f%%)" % (results['accuracy'].mean()*100, 
-                results['accuracy'].std()*100))
+        print("CrossVal Accuracy: %.2f%% (%.2f%%)" % (results['test_accuracy'].mean()*100, 
+                results['test_accuracy'].std()*100))
 
-        print("F1 Score: %.2f (%.2f%%)" % (results['f1_score'].mean()*100, 
-                results['f1_score'].std()*100))
+        print("F1 Score: %.2f (%.2f%%)" % (results['test_f1_score'].mean()*100, 
+                results['test_f1_score'].std()*100))
+
+    def gridSearch(self):
+        scoring = {'accuracy' : make_scorer(accuracy_score)}
+
+                
+        kfold = StratifiedKFold(n_splits=10, random_state=7)
+        search_space = [
+                        {"n_estimators": [100, 200, 300, 400], 
+                         "max_depth": [3, 4, 5, 6],
+                         "learning_rate": [0.3, 0.1, 0.01],
+                         "colsample_bytree": [1.0, 0.9]}
+                        ]
+        grid = GridSearchCV(estimator=self.model, param_grid = search_space, cv=kfold, scoring='accuracy', verbose=1)
+        grid.fit(self.X_train, self.y_train)
+        with open('grid.p', 'wb') as f:
+             pickle.dump(grid, f) 
+        best = grid.best_estimator
+        y_pred = best.predict(self.X_test)                                   
+        predictions = [round(value) for value in y_pred]
+        accuracy = accuracy_score(self.y_test, predictions)
+        print("Accuracy is: "+ accuracy)
+
+        print(grid.results)
+        print(grid.best_estimator)
+        print(grid.best_score)
+        print(grid.best_params)        
+         
